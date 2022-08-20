95ff9f442be23f66f05a491fa32c928893ecee63
Penguin <penguingeek1@gmail.com>
2022-08-20 19:17:00 +0100

    Change Syncronous AJAX requests to async, and automate the # of lines text

--- public/js/bingo.js
+++ public/js/bingo.js
@@ -1,6 +1,6 @@
 Echo.channel(`quiz`)
-    .listen('.bingo', () => {
-        showSheet();
+    .listen('.bingo', (data) => {
+        showSheet(data);
     })
 
 window.onload = function() {
@@ -12,88 +12,85 @@ function showSheet(data) {
     const url_string = window.location.href;
     const url = new URL(url_string);
     const sheet = url.searchParams.get("sheet");
-    const lines = parseInt(url.searchParams.get("line"));
-    let bingoData;
-    let bingo;
+    let lines = 0;
 
-    jQuery.ajax({
-        url: 'https://ehuquizsociety.com/api/bingo/sheet/' + sheet,
-        success: function (result) {
-            bingoData = result;
-        },
-        async: false
-    });
+    if(data) {
+        lines = parseInt(data.lines);
+    } else {
+        lines = parseInt(url.searchParams.get("line"));
+    }
 
     jQuery.ajax({
         url: 'https://ehuquizsociety.com/api/bingo/',
-        success: function (result) {
-            bingo = result;
-        },
-        async: false
-    });
-
-    //Sort cards by x away
-    if(lines) {
-        for(let i = 0; i < 6; i++) {
-            bingoData[i]['away'] = calculateAway(bingoData[i], bingo, lines);
-        }
-
-        // sort by salary
-        bingoData.sort(function (x, y) {
-            return x.away - y.away;
-        });
-    }
-
-    const ctx = document.getElementById('bingo-canvas').getContext('2d');
-    const img = new Image();
-    img.onload = () => {
-        ctx.drawImage(img, 0, 0);
-        let x = 38; //X position of number
-        let y = 63;
-        let identifier_y = 20;
-        let away_y = 192;
-
-        for(let i = 0; i < bingoData.length; i++) {
-            //Add in Sheet Identifier
-            ctx.font = '20px sunday';
-            ctx.fillStyle = "white";
-            ctx.fillText("Sheet: " + bingoData[i].sheet, 50, identifier_y);
-            ctx.fillText("Code: " + bingoData[i].code, 350, identifier_y);
-            identifier_y += 201;
-
-            //Insert numbers for top row
-            for (let i2 = 0; i2 < bingoData[i].data.one.length; i2++) {
-                insertNumber(ctx, bingo, bingoData[i].data.one[i2], x, y);
-                x += 49;
-            }
-
-            x = 38;
-            y += 50;
-            //Insert numbers for middle row
-            for (let i2 = 0; i2 < bingoData[i].data.two.length; i2++) {
-                insertNumber(ctx, bingo, bingoData[i].data.two[i2], x, y);
-                x += 49;
-            }
-
-            x = 38;
-            y += 50;
-            //Insert numbers for middle row
-            for (let i2 = 0; i2 < bingoData[i].data.three.length; i2++) {
-                insertNumber(ctx, bingo, bingoData[i].data.three[i2], x, y);
-                x += 49;
-            }
-
-            //Only calculate x away if there are lines provided, this prevents un-necessary processing at the start of the game
+    }).then(function (bingo) {
+        jQuery.ajax({
+            url: 'https://ehuquizsociety.com/api/bingo/sheet/' + sheet,
+        }).then(function (bingoData) {
+            console.log(bingoData);
+            //Sort cards by x away
             if(lines) {
-                awayCounter(bingoData[i], ctx, bingo, lines, away_y);
+                for(let i = 0; i < 6; i++) {
+                    bingoData[i]['away'] = calculateAway(bingoData[i], bingo, lines);
+                }
+
+                // sort by salary
+                bingoData.sort(function (x, y) {
+                    return x.away - y.away;
+                });
             }
 
-            away_y += 201;
-            x = 38;
-            y += 101;
-        }
-    };
-    img.src = '/images/BingoSheetBackground.png';
+            const ctx = document.getElementById('bingo-canvas').getContext('2d');
+            const img = new Image();
+            img.onload = () => {
+                ctx.drawImage(img, 0, 0);
+                let x = 38; //X position of number
+                let y = 63;
+                let identifier_y = 20;
+                let away_y = 192;
+
+                for(let i = 0; i < bingoData.length; i++) {
+                    //Add in Sheet Identifier
+                    ctx.font = '20px sunday';
+                    ctx.fillStyle = "white";
+                    ctx.fillText("Sheet: " + bingoData[i].sheet, 50, identifier_y);
+                    ctx.fillText("Code: " + bingoData[i].code, 350, identifier_y);
+                    identifier_y += 201;
+
+                    //Insert numbers for top row
+                    for (let i2 = 0; i2 < bingoData[i].data.one.length; i2++) {
+                        insertNumber(ctx, bingo, bingoData[i].data.one[i2], x, y);
+                        x += 49;
+                    }
+
+                    x = 38;
+                    y += 50;
+                    //Insert numbers for middle row
+                    for (let i2 = 0; i2 < bingoData[i].data.two.length; i2++) {
+                        insertNumber(ctx, bingo, bingoData[i].data.two[i2], x, y);
+                        x += 49;
+                    }
+
+                    x = 38;
+                    y += 50;
+                    //Insert numbers for middle row
+                    for (let i2 = 0; i2 < bingoData[i].data.three.length; i2++) {
+                        insertNumber(ctx, bingo, bingoData[i].data.three[i2], x, y);
+                        x += 49;
+                    }
+
+                    //Only calculate x away if there are lines provided, this prevents un-necessary processing at the start of the game
+                    if(lines) {
+                        awayCounter(bingoData[i], ctx, bingo, lines, away_y);
+                    }
+
+                    away_y += 201;
+                    x = 38;
+                    y += 101;
+                }
+            };
+            img.src = '/images/BingoSheetBackground.png';
+        });
+    });
 }
 
 function insertNumber(ctx, bingo, value, x, y) {
